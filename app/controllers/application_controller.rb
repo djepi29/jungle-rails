48bdba3d993afa1b253f3bc21ef868dba132bea5
djepi29 <j3anpi3rr3g3org3s@email.com>
2023-07-09 16:56:49 -0400

    user authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,4 +29,12 @@ class ApplicationController < ActionController::Base
     }
     cookies[:cart]
   end
+
+  helper_method :current_user
+
+  private
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
 end
