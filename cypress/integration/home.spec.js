e62a0fe6ec1427312650ed18f10e2761ec4579d1
djepi29 <j3anpi3rr3g3org3s@email.com>
2023-07-21 14:02:25 -0400

    spec: home_page

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,16 @@
+describe('Home Page', () => {
+
+  it('should visit the home page', () => {
+    // Visit the home page
+    cy.visit('http://localhost:3000/');
+  });
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There is 12 products on the page", () => {
+    cy.get(".products article").should("have.length", 12);
+  });
+  
+});
